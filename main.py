c4f3da82eba5370e72cbdbb97f489e61b0fe3c5a
Magner <magner85@mail.ru>
2021-11-12 21:10:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import DiscordApi
+import discord
 import asyncio
 import discord_command
 
 async def main():
-    bot = DiscordApi.MyClient('NzMzMDA1MDYyNDQ3ODI1MDA3.Xw82KQ.zs0WjdpyyyoN6chxt5Euhu1nTno', discord_command.default_handler)
+    bot = discord.MyClient('NzMzMDA1MDYyNDQ3ODI1MDA3.Xw82KQ.zs0WjdpyyyoN6chxt5Euhu1nTno', discord_command.default_handler)
     await bot.start()
     print('ready')
     while True:
